71c4854face80cc1ab4bb6961f982decbe6871d5
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-20 12:49:24 -0300

    Code the configuration of the store and work on redux thunk and add provider to provide that store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// @ts-nocheck
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import continents from './continents/continents';
+
+const rootReducer = combineReducers({ continents });
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+export default store;
