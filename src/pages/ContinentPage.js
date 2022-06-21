069f6bae21e093e9c0f3c83d0bc08e1d6da82aee
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-20 22:33:20 -0300

    Start recycling the headerWidget and the main titleBar for all pages

--- src/pages/ContinentPage.js
+++ src/pages/ContinentPage.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import HeaderCommonWidget from '../components/HeaderCommonWidget';
+import MainTitleBar from '../components/MainTitleBar';
+
+// const URLDWDEATH = '`https://disease.sh/v3/covid-19/continents/${continent}``';
 
 const ContinentPage = () => {
   const { continent } = useParams();
+  const [deathData, setDeathData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://corona.lmao.ninja/v3/covid-19/continents/${continent}`)
+      .then((response) => response.json())
+      .then((data) => setDeathData(data.deaths))
+      .catch((err) => setDeathData(err.message));
+  }, []);
 
   return (
     <>
-      <h2> Hi {continent}</h2>
+      <HeaderCommonWidget location={continent} death={deathData} />
+      <MainTitleBar title="Continent Death" />
     </>
   );
 };
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Continents from '../components/Continents';
-import WorldDeathWidget from '../components/WorldDeathWidget';
-import style from './Homepage.module.css';
+import HeaderCommonWidget from '../components/HeaderCommonWidget';
+import MainTitleBar from '../components/MainTitleBar';
 
-const Homepage = () => (
-  <>
-    <WorldDeathWidget />
-    <div className={style.headerContinentDeaths}>
-      <h2>Continent Deaths</h2>
-    </div>
-    <Continents />
-  </>
-);
+const Homepage = () => {
+  const [deathData, setDeathData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://corona.lmao.ninja/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => setDeathData(data.deaths))
+      .catch((err) => setDeathData(err.message));
+  }, []);
+
+  return (
+    <>
+      <HeaderCommonWidget death={deathData} location="world" />
+      <MainTitleBar title="Continent Death" />
+      <Continents />
+    </>
+  );
+};
 
 export default Homepage;
