746841e54694df0f52b5b5e8a4b937fa6c172d81
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-22 18:41:08 -0300

    TRY v

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 import Homepage from './pages/Homepage';
 import ContinentPage from './pages/ContinentPage';
 import CountryPage from './pages/CountryPage';
-import NoMatch from './pages/NoMatch';
 
 const routes = [
-  {
-    path: '*',
-    title: '',
-    element: <NoMatch />,
-  },
   { path: '/', title: 'Homepage', element: <Homepage />, exact: 'true' },
   {
     path: '/continent/:continent',
